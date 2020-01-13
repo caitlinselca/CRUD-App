404bd04fa7dc18b7a84c89bdc38f25cb40deda77
Caitin Selca <caitlinselca@gmail.com>
2020-01-13 01:03:24 -0500

    displays all students

--- src/AppView.js
+++ src/AppView.js
@@ -4,22 +4,23 @@ const AppView = (props) => {
     const { students, removeStudent, addStudent } = props;
 
     const studentToAdd = {
-        "id": 2,
-        "firstName": "bob",
-        "lastName": "jones",
-        "email": "bobbyboy1234@yahoo.com",
-        "imageUrl": "https://i.imgur.com/GuAB8OE.jpg",
-        "gpa": 3.7,
-        "createdAt": "2018-12-05T23:02:45.270Z",
-        "updatedAt": "2019-06-14T00:15:35.429Z",
-        "campusId": 1
+        "id": 4,
+        "firstName": "First",
+        "lastName": "Last",
+        "email": "bobbyshmurdabish123@yahoo.com",
+        "gpa": 3.7
     }
 
     return (
     <div className="App">
         <header className="App-header">
-            
-            {students.map(student => <div>{student.firstName} </div>)}
+            <button
+                className="search-button"
+				onClick={() => addStudent(studentToAdd)}> Add Student </button>
+             <button
+                className="search-button"
+				onClick={() => alert("You removed a student!")}> Remove Student </button>
+            {students.map(student => <div> Student Id: {student.id}, Name: {student.firstName} {student.lastName}, Gpa: {student.gpa}, Email: {student.email} </div>)}
         </header>
     </div>
     )
