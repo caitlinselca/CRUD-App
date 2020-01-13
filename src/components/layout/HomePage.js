5a73b285b0a9b66c875710dc64667848b12a3e96
Camryn Buonamassa <camrynbuonamassa@gmail.com>
2020-01-13 15:27:39 -0500

    added welcome to homepage

--- src/components/layout/HomePage.js
+++ src/components/layout/HomePage.js
@@ -1,12 +1,15 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
+import Typography from '@material-ui/core/Typography';
 
 class HomePage extends Component {
 
   render() {
     return (
       <div>	
-      	
+     	<Typography variant="h1" component="h2" align="center">
+      		Welcome
+      	</Typography>
       </div>
     )
   }
