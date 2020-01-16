7e07ff4b5541f2ad4c979a73fa4c67b2fd2254f5
Camryn Buonamassa <camrynbuonamassa@gmail.com>
2020-01-16 14:48:12 -0500

    added function to determine display if there are no students or campuses to display

--- src/components/campuses/AllCampusesView.js
+++ src/components/campuses/AllCampusesView.js
@@ -5,22 +5,34 @@ import AddCampusForm from './AddCampusForm';
 export default function AllCampusesView(props) {
 
     const { campuses, removeCampus, addCampus, editCampus } = props;
-
+    const determineDisplay = (students) =>{
+        if(campuses.length === 0){
+            return (<h2 align="center"> No campuses </h2>);
+        }
+        else{
+            return(
+                <div>
+                    <p></p>
+                    <AddCampusForm addCampus={addCampus} /> 
+                    {campuses.map(campus => {
+                        return (
+                            <CampusCardView 
+                                name={campus.name}
+                                id={campus.id}
+                                variant="campus"
+                                removeCampus={removeCampus}
+                                editCampus={editCampus}
+                            ></CampusCardView>
+                        );
+                    })}
+                </div>
+            )
+        }
+    }
+    let display = determineDisplay(campuses);
     return (
-        <div>
-            <p></p>
-            <AddCampusForm addCampus={addCampus} /> 
-            {campuses.map(campus => {
-                return (
-                    <CampusCardView 
-                        name={campus.name}
-                        id={campus.id}
-                        variant="campus"
-                        removeCampus={removeCampus}
-                        editCampus={editCampus}
-                    ></CampusCardView>
-                );
-            })}
+        <div> 
+            {display}
         </div>
-    )
+    );
 }
--- src/components/students/AllStudentsView.js
+++ src/components/students/AllStudentsView.js
@@ -5,26 +5,38 @@ import AddStudentForm from './AddStudentForm';
 export default function AllStudentsView(props) {
 
     const { students, removeStudent, addStudent, editStudent } = props;
-
+    const determineDisplay = (students) =>{
+        if(students.length === 0){
+            return (<h2 align="center"> No students </h2>);
+        }
+        else{
+            return(
+                <div>
+                    <p></p>
+                    <AddStudentForm addStudent={addStudent}>
+                    </AddStudentForm>
+                    {students.map(student => {
+                        return (
+                            <CardView 
+                                firstName={student.firstName}
+                                lastName={student.lastName}
+                                id={student.id}
+                                email={student.email}
+                                gpa={student.gpa}
+                                variant="student"
+                                removeStudent={removeStudent}
+                                editStudent={editStudent}
+                            ></CardView>
+                        );
+                    })}
+                </div>
+            )
+        }
+    }
+    let display = determineDisplay(students);
     return (
-        <div>
-            <p></p>
-            <AddStudentForm addStudent={addStudent}>
-            </AddStudentForm>
-            {students.map(student => {
-                return (
-                    <CardView 
-                        firstName={student.firstName}
-                        lastName={student.lastName}
-                        id={student.id}
-                        email={student.email}
-                        gpa={student.gpa}
-                        variant="student"
-                        removeStudent={removeStudent}
-                        editStudent={editStudent}
-                    ></CardView>
-                );
-            })}
-        </div>
-    )
+    <div> 
+        {display}
+    </div>
+    );
 }
